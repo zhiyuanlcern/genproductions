1fdd40aa78050bb21c6499432105efaf04d27caf
zhiyuanl <zhiyuanl@cern.ch>
2024-06-11 12:54:13 +0200

    add script to run locally

---
+++ bin/Powheg/Jordan_run.sh
@@ -0,0 +1,74 @@
+# python2 ./run_pwg_condor.py -p 0 -i m1050_t/ggH_mssm.input -m gg_H_2HDM -f ggH_1050 -j 10 
+# python2 ./run_pwg_condor.py -p 123 -i m105_t/ggH_mssm.input -m gg_H_2HDM -f ggH_105 -j 10 
+# python2 ./run_pwg_condor.py -p 9 -i m105_t/ggH_mssm.input -m gg_H_2HDM -f ggH_105 -j 10 
+
+# python2 ./run_pwg_condor.py -p 01239 -i cards/m100_t/ggH_mssm.input -m gg_H_2HDM -f ggH_100 -j 10 
+
+#  python2 ./run_pwg_condor.py -p 01239 -i cards/m60_t/ggH_mssm.input -m gg_H_2HDM -f ggH_60 -j 10 &
+#  python2 ./run_pwg_condor.py -p 01239 -i cards/m80_t/ggH_mssm.input -m gg_H_2HDM -f ggH_80 -j 10 &
+# sleep 1500
+#  python2 ./run_pwg_condor.py -p 01239 -i cards/m90_t/ggH_mssm.input -m gg_H_2HDM -f ggH_90 -j 10 &
+#  python2 ./run_pwg_condor.py -p 01239 -i cards/m95_t/ggH_mssm.input -m gg_H_2HDM -f ggH_95 -j 10 &
+# sleep 1500
+#  python2 ./run_pwg_condor.py -p 01239 -i cards/m110_t/ggH_mssm.input -m gg_H_2HDM -f ggH_110 -j 10 &
+#  python2 ./run_pwg_condor.py -p 01239 -i cards/m120_t/ggH_mssm.input -m gg_H_2HDM -f ggH_120 -j 10 &
+# sleep 1500
+
+#  python2 ./run_pwg_condor.py -p 01239 -i cards/m125_t/ggH_mssm.input -m gg_H_2HDM -f ggH_125 -j 10 &
+#  python2 ./run_pwg_condor.py -p 01239 -i cards/m130_t/ggH_mssm.input -m gg_H_2HDM -f ggH_130 -j 10 &
+# sleep 1500
+#  python2 ./run_pwg_condor.py -p 01239 -i cards/m140_t/ggH_mssm.input -m gg_H_2HDM -f ggH_140 -j 10 &
+
+
+# python3 ./run_pwg_condor.py -p 123 -i cards/m60_t/ggH_mssm.input -m gg_H_2HDM -f ggH_60 -j 10
+# python3 ./run_pwg_condor.py -p 0 -i cards/m60_t/ggH_mssm.input -m gg_H_2HDM -f ggH_60 -j 10
+
+
+
+# 160 180 200 250 300 350 400 450 500 600 700 800 1000 1100 1200 1400 1600 1800 2000 2300 2600 2900 3200 3500;
+
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m60_b/ggH_mssm.input -m gg_H_2HDM -f ggH_60_b -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m60_tb/ggH_mssm.input -m gg_H_2HDM -f ggH_60_tb -j 10 &
+
+# # python3 ./run_pwg_condor.py -p 01239 -i cards/m100_b/ggH_mssm.input -m gg_H_2HDM -f m100_b  -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_b_hfactDown/ggH_mssm.input -m gg_H_2HDM -f m100_b_hfactDown  -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_b_hfactUp/ggH_mssm.input -m gg_H_2HDM -f m100_b_hfactUp  -j 10 &
+# # python3 ./run_pwg_condor.py -p 01239 -i cards/m100_tb/ggH_mssm.input -m gg_H_2HDM -f m100_tb  -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_tb_hfactDown/ggH_mssm.input -m gg_H_2HDM -f m100_tb_hfactDown  -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_tb_hfactUp/ggH_mssm.input -m gg_H_2HDM -f m100_tb_hfactUp  -j 10 &
+# python3 ./run_pwg_condor.py -p 0 -i cards/m60_t/ggH_mssm.input -m gg_H_2HDM -f ggH_60 -j 10
+
+
+
+
+
+
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_tb_higgs1_hfactDown/ggH_mssm.input -m gg_H_2HDM -f m100_tb_higgs1_hfactDown  -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_tb_higgs1_hfactUp/ggH_mssm.input -m gg_H_2HDM -f m100_tb_higgs1_hfactUp  -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_tb_higgs1/ggH_mssm.input -m gg_H_2HDM -f m100_tb_higgs1 -j 10 &
+
+
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_b_higgs1/ggH_mssm.input -m gg_H_2HDM -f m100_b_higgs1  -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_b_higgs1_hfactDown/ggH_mssm.input -m gg_H_2HDM -f m100_b_higgs1_hfactDown  -j 10 &
+python3 ./run_pwg_condor.py -p 01239 -i cards/m100_b_higgs1_hfactUp/ggH_mssm.input -m gg_H_2HDM -f m100_b_higgs1_hfactUp  -j 10 &
+
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_t_higgs1_hfactUp/ggH_mssm.input -m gg_H_2HDM -f m100_t_higgs1_hfactUp -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_t_higgs1_hfactDown/ggH_mssm.input -m gg_H_2HDM -f m100_t_higgs1_hfactDown -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_t_higgs1/ggH_mssm.input -m gg_H_2HDM -f m100_t_higgs1 -j 10 &
+
+
+
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_tb_higgs3_hfactDown/ggH_mssm.input -m gg_H_2HDM -f m100_tb_higgs3_hfactDown  -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_tb_higgs3_hfactUp/ggH_mssm.input -m gg_H_2HDM -f m100_tb_higgs3_hfactUp  -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_tb_higgs3/ggH_mssm.input -m gg_H_2HDM -f m100_tb_higgs3 -j 10 &
+
+
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_b_higgs3/ggH_mssm.input -m gg_H_2HDM -f m100_b_higgs3  -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_b_higgs3_hfactDown/ggH_mssm.input -m gg_H_2HDM -f m100_b_higgs3_hfactDown  -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_b_higgs3_hfactUp/ggH_mssm.input -m gg_H_2HDM -f m100_b_higgs3_hfactUp  -j 10 &
+
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_t_higgs3_hfactUp/ggH_mssm.input -m gg_H_2HDM -f m100_t_higgs3_hfactUp -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_t_higgs3_hfactDown/ggH_mssm.input -m gg_H_2HDM -f m100_t_higgs3_hfactDown -j 10 &
+# python3 ./run_pwg_condor.py -p 01239 -i cards/m100_t_higgs3/ggH_mssm.input -m gg_H_2HDM -f m100_t_higgs3 -j 10 &
+
+
